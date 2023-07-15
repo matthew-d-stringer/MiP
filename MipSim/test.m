6657e0e7e96434af8dcd868ab08c7b724f5eb049
mdsflyboy <mdsflyboy@gmail.com>
2023-07-15 13:30:38 -0700

    Adding input saturation to model

--- MipSim/test.m
+++ MipSim/test.m
@@ -1,29 +1,13 @@
 clear; close all; clc;
 sim = MipSim;
 
-x0 = [0.5 0 0 0]';
+x0 = [1.5 0 0 0]';
     
-% A = sim.linearized(x0, 0)
 [A,B] = sim.linearizedInverted();
 
 [K,S,P] = lqr(A, B, eye(4), 2);
 
-sim.xyRange([-4 10]);
-xVal=[-4 10];
-bottom = -1;
-sim.axisVals = [xVal(1) xVal(2) bottom bottom+xVal(2)-xVal(1)];
+sim.xyRange([-4 10], -1);
 
-% K
-
-% sim.axisVals
-
-% sim.animateWithConstantTorque(x0, 1,0.05);
-
-% sim.K1 * sim.K3 * sim.K2^(-2)
-
-% x = sim.run(x0, @(x) 0.1, 0.01, 10);
-
-TF = sim.linearizedTransferFunction()
-
-% frames = sim.animateWithComputedU(x0, @(x) -K*x, 0.01, 4);
-% sim.saveAnimation('Animation.avi', frames, 0.01);
+frames = sim.animateWithComputedU(x0, @(x) -K*x, 0.01, 4);
+% sim.saveAnimation('Animation.avi', frames);
